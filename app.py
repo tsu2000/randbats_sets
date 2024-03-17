e8689095fe9aa78e8403293de6eecfd9c631c98e
tsu2000 <106811131+tsu2000@users.noreply.github.com>
2024-03-17 16:30:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -59,7 +59,9 @@ def main():
         # Set row index names
         df.rename(index = {'abilities': 'Ability', 'items': 'Possible Held Items', 'teraTypes': 'Possible Tera Types', 'moves': 'Possible Moves'}, inplace = True)
 
-        st.dataframe(df)
+        st.dataframe(df,
+                     column_config = {"Values": st.column_config.TextColumn(width = "large")}
+                    )
     
     
 if __name__ == "__main__":
